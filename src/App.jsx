673ce90015448464e530166775373682da3f0d4f
abobakaraslam <164588250+abobakaraslam@users.noreply.github.com>
2025-04-19 15:19:08 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+/* File: App.jsx */
+function App() {
+  let variableX = 100;
+  return (
+    <div>
+      <h1>Value of simple variable in JavaScript: {variableX}</h1>
+    </div>
+  );
+}
+export default App;
